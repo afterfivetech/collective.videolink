f83fc3baf7844f35b39e521f5695977aece18636
David Bain <pigeonflight@gmail.com>
2017-12-31 13:44:11 +0000

    added the interfaces.py file with our custom interfaces

---
+++ collective/videolink/interfaces.py
@@ -0,0 +1,9 @@
+from zope.interface import Interface
+
+class IVideoLinkOembedable(Interface):
+    "maker interface for oembedable videos"
+    pass
+
+class IVideoLinkThumb(Interface):
+    "maker interface for video links"
+    pass
